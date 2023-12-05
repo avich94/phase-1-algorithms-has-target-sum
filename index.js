719e9e3f2c89f6569c48f6325d5a6b7f2ead0290
Austin Vichich <avichich4@gmail.com>
2023-12-04 18:08:02 -0700

    completed lab

--- index.js
+++ index.js
@@ -1,17 +1,55 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  const sortedArray = array.sort((a, b) => a - b);
+  let startIndex = 0;
+  let endIndex = array.length - 1;
+
+  while (startIndex < endIndex) {
+    const sum = sortedArray[startIndex] + sortedArray[endIndex];
+    if (sum === target) {
+      return true;
+    } 
+    if (sum < target) {
+      startIndex++;
+    } else {
+      endIndex--;
+    }
+  }
+
+  return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
+
+O(n)
+
 */
 
 /* 
   Add your pseudocode here
-*/
+  initialize two variables used as pointers in the array
+  
+  sort the array in ascending order
+
+  start a while loop that checks if the start index is less than the end index
+
+    declare a variable that is assigned to the value of start poiner + end pointer
+
+    when the start pointer is less than the end pointer continue to loop
+
+    initialize sum which is assigned to the value of pointers index location added together
+      if the sum === target return true
+      if the sum is less than the target increment the start index up one
+      else decrement the end pointer one value and repeat the while loop
+
 
 /*
   Add written explanation of your solution here
+  This code first sorts the array in ascending value. It initializes two pointers which start on each end of the array.
+  When the start pointer is less than the end pointer it initiates the loop. This allows the function to loop through each index and compare if they are
+  equal to the sum value. If the sum is equal to the target value it returns true. If it is not it will increment the start index as long as the sum is less than the 
+  target. If the sum is more than the end pointer will decrement and the loop will restart. If the function iterates through the whole array without a sum equal to the target
+  it will return false.
 */
 
 // You can run `node index.js` to view these console logs
@@ -29,6 +67,11 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([18, 4, 8], 9));
 }
 
 module.exports = hasTargetSum;
